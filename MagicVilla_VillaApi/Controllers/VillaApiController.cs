cf98465b2e739512970f32eec9db1c9a016806b8
Ahmed Alkali Mohammed <hmkalkali@gmail.com>
2024-10-11 18:51:52 +0200

    Implemented Logger

--- MagicVilla_VillaApi/Controllers/VillaApiController.cs
+++ MagicVilla_VillaApi/Controllers/VillaApiController.cs
@@ -2,14 +2,28 @@
 using MagicVilla_VillaApi.Models;
 using MagicVilla_VillaApi.Models.Dto;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
 
 namespace MagicVilla_VillaApi.Controllers
 {
     [Route("api/VillaAPI")]  //We add a route name here, it could also be [Route("api/[controller]")]
-    [ApiController]
+    [ApiController]  //this helps us with validation as well it does it by defualt
     public class VillaApiController : ControllerBase  //Rember to import controllerbase
     {
+
+        //In order to implement logger 
+
+        private readonly ILogger<VillaApiController> _logger;
+
+        public VillaApiController(ILogger<VillaApiController> logger) 
+        {
+            _logger = logger;
+        }
+
+
+
         [HttpGet] //Tell us the type of request
         /*To be able add status codes we add the Actionresult and wrap it around the function name 
          * then in the return statement we add Ok(what want to return)
@@ -26,6 +40,7 @@ namespace MagicVilla_VillaApi.Controllers
 
             //we store the data inside the VillaStore class now we are calling it  instead of what we had before
             //return VillaStore.villaList;
+            _logger.LogInformation("Getting All Villas");
             return Ok(VillaStore.villaList);
 
 
@@ -48,6 +63,7 @@ namespace MagicVilla_VillaApi.Controllers
             //lets add validation
             if(id == 0)
             {
+                _logger.LogError("Get Villa Error with Id" + id);
                 return BadRequest();
             }
             var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
@@ -64,12 +80,20 @@ namespace MagicVilla_VillaApi.Controllers
 
 
         [HttpPost]
-        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         //here we say [frombody] its exactly ike req.body
         public ActionResult<VillaDto> CreateVilla([FromBody]VillaDto villaDto)
         {
+            //Adding custom validation and checking if villa exist already
+            if(VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower() == villaDto.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("Custom Error", "Villa already Exists!");
+                return BadRequest(ModelState);
+            }
+
+
             if(villaDto == null)
             {
                 return BadRequest(villaDto);
@@ -86,5 +110,74 @@ namespace MagicVilla_VillaApi.Controllers
             return CreatedAtRoute("GetVilla", new { id = villaDto.Id }, villaDto);
 
         }
+
+
+        [HttpDelete("id", Name="DeleteVilla")]
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        //With IActionResult it returns Nocontent which perfect for delete request unlike just Action Result which will return other status codes
+        public IActionResult DeleteVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            //the u => u.Id == id checks if the parameter passed is matches an id in our store so acts like a for loop
+            //return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            VillaStore.villaList.Remove(villa);
+            return NoContent();
+        }
+
+
+        [HttpPut("{id:int}", Name = "UpdateVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult UpdateVilla(int id, [FromBody]VillaDto villaDto)
+        {
+            if(villaDto == null || id != villaDto.Id)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+
+            villa.Name = villaDto.Name;
+            villa.Sqft = villaDto.Sqft;
+            villa.Occupancy = villaDto.Occupancy;
+
+            return NoContent();
+        }
+
+
+        [HttpPatch("{id:int}", Name = "UpdatePartialVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public IActionResult UpdatePartialVilla(int id, JsonPatchDocument<VillaDto> patchDto)
+        {
+            if (patchDto == null || id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if(villa == null)
+            {
+                return BadRequest();
+            }
+            patchDto.ApplyTo(villa, ModelState);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
+        
     }
 }
--- MagicVilla_VillaApi/Models/Dto/VillaDto.cs
+++ MagicVilla_VillaApi/Models/Dto/VillaDto.cs
@@ -1,8 +1,16 @@
-﻿namespace MagicVilla_VillaApi.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaApi.Models.Dto
 {
     public class VillaDto
     {
         public int Id { get; set; }
+        [Required] //this makes sure the field is required 
+        [MaxLength(30)] //self explantory
         public string Name { get; set; }
+
+        public int Occupancy { get; set; }
+
+        public int Sqft { get; set; }
     }
 }

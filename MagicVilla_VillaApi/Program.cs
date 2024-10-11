611c6b8f721300562077181d34d7b84ba47dd29c
Ahmed Alkali Mohammed <hmkalkali@gmail.com>
2024-10-11 19:16:38 +0200

    Comments

--- MagicVilla_VillaApi/Program.cs
+++ MagicVilla_VillaApi/Program.cs
@@ -1,6 +1,15 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+//For logger services
+//Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.File("log/villaLogs.txt", rollingInterval:RollingInterval.Day).CreateLogger();
+
+//tells the app to not use the defualt logger but the seri logger 
+//builder.Host.UseSerilog();
+
 
 builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
